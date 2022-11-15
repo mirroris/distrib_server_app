81278f07010674167427c10bebeedf224a6306be
mirroris <u455017k@ecs.osaka-u.ac.jp>
2022-11-16 02:34:35 +0900

    finish http.go

--- internal/server/http.go
+++ internal/server/http.go
@@ -63,4 +63,30 @@ func(s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request){
 		http.Error(wk err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request){
+		defer r.Body.Close()
+
+		var req ConsumeRequest
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		record, err := s.Log.Read(req.Offset)
+		if err ErrOffsetNotFound {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		res := ConsumeResponse{Record: record}
+		err = json.NewEncoder(w).Encode(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
 }
